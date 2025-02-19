4887862e97e99aa63591c12538d480295d9baf39
Sajmul <sajmul1427@gmail.com>
2025-02-19 17:22:55 +0600

    task card color changed for dark mode

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -10,7 +10,7 @@ const Task = ({task}) => {
   return (
     <Link
       to={`/dashboard/tasks/details/${id}`}
-      className="relative task bg-second-color/10 text-white rounded-lg shadow-lg hover:shadow-xl transform hover:scale-105 transition-all duration-300 p-4 overflow-hidden block"
+      className="relative bg-second-color/10 text-white rounded-lg shadow-lg hover:shadow-xl transform hover:scale-105 transition-all duration-300 p-4 overflow-hidden block"
     >
       <div className="absolute inset-0 bg-gradient-to-br from-main-color via-second-color to-main-color opacity-5"></div>
       <div className="absolute -top-10 -right-10 w-40 h-40 bg-second-color opacity-20 rounded-full"></div>
@@ -21,18 +21,18 @@ const Task = ({task}) => {
         alt={title}
         className="w-full h-60 object-cover rounded-lg mb-4"
       />
-      <h2 className="text-xl text-black font-bold mb-2">{title}</h2>
-      <p className="text-gray-600 mb-1">
+      <h2 className="text-xl text-black dark:text-white font-bold mb-2">{title}</h2>
+      <p className="text-gray-600 dark:text-gray-300 mb-1">
         <span className="font-semibold">Required Workers:</span> <span>{workers}</span>
       </p>
-      <p className="text-gray-600 mb-1">
+      <p className="text-gray-600 dark:text-gray-300 mb-1">
         <span className="font-semibold">Payable Amount:</span> <span>{amount}$</span>
       </p>
-      <p className="text-gray-600 mb-1">
+      <p className="text-gray-600 dark:text-gray-300 mb-1">
         <span className="font-semibold">Last Submit Date:</span>{" "}
         <span>{format(new Date(date), "PP")}</span>
       </p>
-      <p className="text-gray-600 mb-3">
+      <p className="text-gray-600 dark:text-gray-300 mb-3">
         <span className="font-semibold">Buyer:</span> <span>{buyer?.name}</span>
       </p>
       <button className="bg-second-color text-white btn mt-4">
